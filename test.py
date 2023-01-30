57059c612ab1deb6f97e2b3c48b8a70326f012fb
Venkata Saketh Teki <58323545+tekisaketh@users.noreply.github.com>
2023-01-30 19:10:12 +0100

    Add files via upload

---
+++ test.py
@@ -0,0 +1,21 @@
+import requests
+import pandas
+import json
+
+url = 'https://scanr-api.enseignementsup-recherche.gouv.fr/api/v2/publications/search'
+
+
+searchreq = {"lang":"en","searchFields":["summary","title"],"query":"deep learning","page":0,"pageSize":1}
+#searchreq = {"lang":"en","page":0,"pageSize":1,}
+
+#requesting the API for the data using the search request parameter
+payload = requests.post(url, json=searchreq)
+
+json_pl = payload.json()
+info  = json_pl.get('results')[0].get('value')
+
+
+#Dumping the payload in a file to test the data
+file1 = open("./Code/output.txt",'w')
+file1.write(str(info))
+file1.close()
